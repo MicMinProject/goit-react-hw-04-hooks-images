c85eafb13ded6afa581204cd1e29bfc36d4e8bee
MicMinProject <67431264+MicMinProject@users.noreply.github.com>
2022-03-23 21:43:13 +0100

    portal mod

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,42 +1,64 @@
 import React from 'react';
+import PortalModal from './PortalModal.jsx';
+import PropTypes from 'prop-types';
 /** @jsxImportSource @emotion/react */
 import { jsx } from '@emotion/react';
 import KeyboardEventHandler from 'react-keyboard-event-handler';
 
-const Modal = ({image, onClick, onKeyPress}) =>{
-  return(
-    <div 
-      onClick={onClick}
-      css={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'rgba(0, 0, 0, 0.8)',
-        zIndex: 1200,
-      }}
-      >
-      <KeyboardEventHandler onKeyEvent={onKeyPress} handleKeys={['esc']} />
-      <div
+class Modal extends React.Component {
+  
+  render(){
+
+    const {image, onClick, onKeyPress} = this.props;
+
+    const modal = image !== '' ? (
+      <PortalModal>
+        <div 
+        onClick={onClick}
         css={{
-          maxWidth: 'calc(100vw - 48px)',
-          maxHeight: 'calc(100vh - 24px)',
-        }}>
-        <img 
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          zIndex: 1200,
+        }}
+        >
+        <KeyboardEventHandler onKeyEvent={onKeyPress} handleKeys={['esc']} />
+        <div
           css={{
-            width: '900px',
-            height: '600px',
-            objectFit: 'cover',
-          }}
-          src={image} 
-          alt="large size of result" />
+            maxWidth: 'calc(100vw - 48px)',
+            maxHeight: 'calc(100vh - 24px)',
+          }}>
+          <img 
+            css={{
+              width: '900px',
+              height: '600px',
+              objectFit: 'cover',
+            }}
+            src={image} 
+            alt="large size of result" />
+        </div>
       </div>
-    </div>
-  )
+      </PortalModal>
+    ) : null
+  
+    return(
+      <div>
+        {modal}
+      </div>
+    )
+  }
+}
+
+Modal.propTypes = {
+  image: PropTypes.string,
+  onClick: PropTypes.func,
+  onKeyPress: PropTypes.func,
 }
 
 export default Modal;
